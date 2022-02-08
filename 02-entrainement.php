0fb798fa5f555c90b01b269c6241c886782f3df5
Mariam-26 <82433410+Mariam-26@users.noreply.github.com>
2022-02-08 20:58:24 +0100

    coursPHP

---
+++ 02-entrainement.php
@@ -0,0 +1,12 @@
+<?php 
+/*commentire multiligne */
+// commentaire monoligne
+echo 'Coucou';
+
+print 'PHP is fun';
+
+echo '<h1> Bonjour le monde</h1>';
+
+?>
+<h2><?php echo "Quelle météo splendide"; ?> ! </h2>
+<p><?php echo `nous sommes`; ?>Lundi</p>
