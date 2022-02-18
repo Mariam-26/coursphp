d7dc89be03ab869d1f65674be7917a7ec10bf16a
Mariam-26 <82433410+Mariam-26@users.noreply.github.com>
2022-02-18 13:48:38 +0100

    coursphp

---
+++ inc/function.php
@@ -0,0 +1,12 @@
+<?php 
+ function jevar_dump($mavariable){
+   echo "<pre class=\"alert alert-success\">var_dump :"; 
+   var_dump($mavariable);
+    echo"</pre>";
+ }
+
+
+ function jeprint_r($mavariable){
+   echo "<pre class=\"alert alert-success\">print_r :"; print_r($mavariable); 
+   echo" </pre>";
+ }
