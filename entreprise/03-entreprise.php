31acf7df09906f214aac5318cc1458f10e0da355
Mariam-26 <82433410+Mariam-26@users.noreply.github.com>
2022-02-25 16:20:30 +0100

    coursphp

--- entreprise/03-entreprise.php
+++ entreprise/03-entreprise.php
@@ -1,6 +1,6 @@
-<?php 
+<?php
 // Je définie le titre
-$titre = "Employé Ouédraogo";
+$titre = "mariam Ouédraogo";
 
 //  INCLUSION DU HEADER 
 require_once '../entreprise/includes/header_entreprise.php'; 
@@ -9,63 +9,32 @@ require_once '../entreprise/includes/header_entreprise.php';
 require_once '../entreprise/connect.php';
 ?>
 
+
+
 <!-- MAIN -->
 <main class="container">
   <div class="row col-12">
       <div class="col-lg-12 col-md-12 col-sm-12">
-      <h1 class="p-5 text-center">Employé Ouédraogo</h1>
-          
+      <h1 class="p-5 text-center">mariam Ouédraogo<h1>         
       <div class="row">
-    <?php 
-
-    // MA REQUETE SQL
-
-
-
+        
    
-    if(isset($_GET['id_employes'])){
-      // ici on verifie que l'id du commentaire existe ds notre BDD
-      $resultat = $pdoEntreprise->prepare("SELECT * FROM employes WHERE id_employes = :id_employes");
-      $resultat->execute(array(
-        ':id_employes' => $_GET['id_employes'] /* on associe id_employes à l'id récupéré ds le lien de la page */
-      ));
-      if ($resultat->rowCount() == 0) {/* si l'id_commentaire n'existe pas ds la BDD alors je renvoie vers une autre page */
-        // header('location:02-entreprise.php'); /* on redirige vers la page de départ */ 
-        exit(); /* on arrête le script car il ne correspond à rien */
-      }
-      $fiche = $resultat->fetch(PDO::FETCH_ASSOC);
-      }else { /* si j'arrive sur la page sans rien dans l'URL */
-        // header('location:02-entreprise.php'); 
-        exit();
-      }          
-   
+      </div>
 
-
-
-
-
-
-
-            //  $requete = $pdoEntreprise ->query ( "SELECT * FROM employes WHERE prenom='emilie'"); 
-            //  $ligne = $requete->fetch(PDO:: FETCH_ASSOC);
-            //  jeprint_r($ligne); 
-            
-              ?>
-              </div>
-
-              <!-- CARD  -->
+      <!-- CARD  -->
       <div class="col-12 col-md-6">   
       <div class="card">
+        
           <div class="card-header">
-            ID de l'employé' : <?php echo $fiche['id_employes']; ?>
+            ID de l'employé : <?php echo $fiche['id_employes'];?>
           </div>
           <div class="card-body">
-            <h4 class="card-title">Prenom : <?php echo $fiche['prenom']; ?></h4>
-            <p class="card-text">nom : <?php echo $fiche['nom']; ?></p>
-            <p class="card-text">sexe : <?php echo $fiche['sexe']; ?></p>
-            <p class="card-text">service : <?php echo $fiche['service']; ?></p>
-            <p class="card-text">salaire : <?php echo $fiche['salaire']; ?></p>
-            <p class="card-text">date_embauche : <?php echo $fiche['date_embauche']; ?></p>
+            <h4 class="card-title">Prenom : <?php echo $fiche['prenom'];?></h4>
+            <p class="card-text">nom : <?php echo $fiche['nom'];?></p>
+            <p class="card-text">sexe : <?php echo $fiche['sexe'];?></p>
+            <p class="card-text">service : <?php echo $fiche['service'];?></p>
+            <p class="card-text">salaire : <?php echo $fiche['salaire'];?></p>
+            <p class="card-text">date_embauche : <?php echo $fiche['date_embauche'];?></p>
           </div>
           <div class="card-footer text-muted">
             Date d'embauchement ou de mise à jour : <?php echo date('d/m/y', strtotime($fiche['date_enregistrement'])); ?>
@@ -73,37 +42,9 @@ require_once '../entreprise/connect.php';
         </div>
         </div>
 
-        <!-- FIN FORMULAIRE DE LA MISE A JOUR -->
-
-        <?php 
-          // 
-          if(!empty($_POST)){/* Je vérifie que mon formulaire n'est ps vide  */
-
-            // Instruction pour contrer carrer toutes failles
-            $_POST['prenom'] = htmlspecialchars($_POST['prenom']);
-            $_POST['nom'] = htmlspecialchars($_POST['nom']); 
-            $_POST['sexe'] = htmlspecialchars($_POST['sexe']); 
-            $_POST['service'] = htmlspecialchars($_POST['service']);
-            $_POST['salaire'] = htmlspecialchars($_POST['salaire']); 
-
-            $resultat = $pdoEntreprise->prepare("UPDATE employes SET prenom = :prenom, nom = :nom, sexe = :sexe, service = :service, salaire = :salaire WHERE id_employes = :id_employes");
-
-            $resultat->execute(array(
-              ':id_employes' => $_GET['id_employes'],
-              ':prenom' => $_POST['prenom'],
-              ':nom' => $_POST['nom'],
-              ':sexe' => $_POST['sexe'],
-              ':service' => $_POST['service'],
-              ':salaire' => $_POST['salaire']
-            ));
-            header('location:02-entreprise.php');
-            exit();
-          }
-          ?>
-        
         <!-- FORMULAIRE DE LA MISE A JOUR -->
         <div class="col-12 col-md-6 m-auto">
-          <h2 class="p-5 ">Mise à jour du dernier salarié</h2>
+          <h2 class="p-5 ">Mise à jour du  salarié</h2>
           <form action="#" method="POST" class="mb-5">
 
             <div class="mb-3">
@@ -140,8 +81,34 @@ require_once '../entreprise/connect.php';
             Mise à jour du salarié
             </button>
           </form>
-          
-        
+           <!-- FIN FORMULAIRE DE LA MISE A JOUR -->
+
+          <?php 
+          // 
+          if(!empty($_POST)){/* Je vérifie que mon formulaire n'est ps vide  */
+
+            // Instruction pour contrer carrer toutes failles
+            $_POST['prenom'] = htmlspecialchars($_POST['prenom']);
+            $_POST['nom'] = htmlspecialchars($_POST['nom']); 
+            $_POST['sexe'] = htmlspecialchars($_POST['sexe']); 
+            $_POST['service'] = htmlspecialchars($_POST['service']);
+            $_POST['salaire'] = htmlspecialchars($_POST['salaire']); 
+
+            $resultat = $pdoEntreprise->prepare("UPDATE employes SET prenom = :prenom, nom = :nom, sexe = :sexe, service = :service, salaire = :salaire WHERE id_employes = :id_employes");
+
+            $resultat->execute(array(
+             
+              ':prenom' => $_POST['prenom'],
+              ':nom' => $_POST['nom'],
+              ':sexe' => $_POST['sexe'],
+              ':service' => $_POST['service'],
+              ':salaire' => $_POST['salaire'],
+             
+            ));
+            // header('location:02-entreprise.php');
+            // exit();
+          }
+          ?>        
         </div>
         </div>
          
