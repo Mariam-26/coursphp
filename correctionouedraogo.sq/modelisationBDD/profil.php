df928808b2eba70e53d931948adbc764bb504347
Mariam-26 <82433410+Mariam-26@users.noreply.github.com>
2022-03-15 19:50:04 +0100

    coursphp

--- correctionouedraogo.sq/modelisationBDD/profil.php
+++ correctionouedraogo.sq/modelisationBDD/profil.php
@@ -1,18 +1,17 @@
 <?php 
 // 
 require_once 'connect.php';
-
-// 2- Traitement du formulaire
-if(!estConnecte()) {/* si la personne ne remplit ps ls conditions de la fonction estConnecte() alors on va la renvoyer vers la page connection.php */
-header('location:connexion.php');
-
-
-
-
-  
+// vérification de connexion
+if (!estConnecte()) {/* Si la personne ne remplit pas les conditions de la fonction estConnecte() alors on va la renvoyer vers la page connexion.php */
+  header('location:connexion.php');
 }
 
+// 3 - Déconnexion
 
+if(isset($_GET['action']) && $_GET['action'] == 'deconnexion'){ /* si ds l'URL je récupère action = déconnexion */
+  unset($_SESSION['membres']);
+  $contenu .= "<div class=\"alert alert-success\">Vous avez bien été déconnecté</div>";
+}
 
 ?>
 
@@ -23,7 +22,7 @@ header('location:connexion.php');
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Blog - Connexion</title>
+  <title>Blog - Profil personnel</title>
   <!-- BOOTSWATCH CSS -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.1.3/lux/bootstrap.min.css" integrity="sha512-B5sIrmt97CGoPUHgazLWO0fKVVbtXgGIOayWsbp9Z5aq4DJVATpOftE/sTTL27cu+QOqpI/jpt6tldZ4SwFDZw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
@@ -32,49 +31,47 @@ header('location:connexion.php');
 
   <div class="p-5 bg-primary">
       <div class="container">
-          <h1 class="display-3 text-white">Profil de <?php echo $_SESSION['membres'] = ['nom']; ?> </h1>
-          <!-- à partir du moment ou un membre est connecté, ses infos st passées ds la superglobale $_SESSION et on peut donc accéder à n'importe quel moment et endroit du site -->
+          <h1 class="display-3 text-white">Profil de <?php echo $_SESSION['membres']['prenom'] . ' ' . $_SESSION['membres']['nom']; ?></h1>
+          <!-- à partir du moment où un membre est connecté, ses infos sont passées dans la superglobale $_SESSION et on peut donc y accéder à n'importe quel moment et endroit du site -->
           <p class="lead text-white">Bienvenue sur votre profil
-            <?php if(estAdmin()){
-              echo "<small>-- Vous êtes admin</small>";
-            }else {
-              echo "<small>-- Vous êtes admin</small>";
-              if($_SESSION['membres']['civilite'] == 'f') {
-                echo 'e';
-              }
-              echo "!</small>";
-            }  ?> 
-            </p>
+              <?php if (estAdmin()) {
+                  echo "<small>-- Vous êtes admin</small>";
+              } else {
+                  echo "<small>-- Vous êtes connecté";
+                  if ($_SESSION['membres']['civilite'] == 'f') {
+                      echo "e";
+                  }
+                  echo " !</small>";
+              } ?>
+          </p>
       </div>
   </div>
-
-
-
-
-
-
-
 
   <div class="container">
       <div class="row">
           <div class="col-8 mx-auto">
-          <!-- <?php echo $contenu; ?>
-              <form action="#" method="POST" class="p-5 my-2 border-primary">
+              <?php echo $contenu; ?>
 
-              <div class="mb-3">
+              <div class="card">
+                <div class="card-body">
+                  <p class="card-text">Civilité : 
+                    <?php if($_SESSION['membres']['civilite'] == 'm'){
+                    echo "Homme";
+                  }else {
+                    echo "Femme";
+                  } ?> </p>
+                  
+                  <address class="card-text fst-italic">Adresse<?php echo $_SESSION['membres']['adresse'] . '<br>' . $_SESSION['membres']['code_postal'] . ' ' . $_SESSION['membres']['ville']; ?></address>
 
+                  <p class="card-text">Pseudo : <?php echo $_SESSION['membres']['pseudo']; ?></p>
 
-    <label for="pseudo">pseudo *</label>
-    <input type="text" name="pseudo" id="pseudo" class="form-control">
+                  <p class="card-text">E-mail : <?php echo $_SESSION['membres']['email']; ?></p>
 
-      </div>
-            <div class="mb-3">
-                <label for="mdp">Mot de passe *</label>
-              <input type="password" name="mdp" id="mdp" class="form-control">
-          </div>
-                  <button type="submit" class="btn btn-primary">S'inscrire</button>
-
-              </form> -->
+                </div>
+                <div class="card-footer text-muted">
+                 <a href="connexion.php?action=deconnexion" class="btn btn-sm  btn-outline-primary">Se déconnecter</a>
+                </div>
+              </div>
 
           </div>
       </div>
